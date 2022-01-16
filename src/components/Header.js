0df2ecd9bc6e465c9bd613af53210b692b494018
advikak <advika.kumari29@gmail.com>
2022-01-15 18:03:45 -0700

    Co-authored-by: kelly-zhu <kelly-zhu@users.noreply.github.com> Co-authored-by: swong77 <swong77@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AddGroup from './AddGroup';
 
 const Header = () => {
     return(
@@ -6,6 +7,7 @@ const Header = () => {
             <div className= "container">
                 <h2>Groupify</h2>
             </div>
+            <AddGroup />
         </div>
     );
 }
