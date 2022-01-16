7870cf288c1f34d2350d101bdbc1058dbc841fb0
advikak <advika.kumari29@gmail.com>
2022-01-15 20:14:03 -0700

    Co-authored-by: kelly-zhu <kelly-zhu@users.noreply.github.com> Co-authored-by: swong77 <swong77@users.noreply.github.com>

---
+++ src/components/GroupList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import GroupCard from "./GroupCard";
+
+const GroupList = (props) => {
+    console.log(props);
+    
+    const renderGroupList = props.groups.map((group) => {
+        return (
+            // <GroupCard group={group}></GroupCard>
+    
+            
+            <div className = "item">
+                <div className = "content">
+                    <div className = "header">{group.name}</div>
+                    <div>{group.type}</div>
+                    <div>{group.link}</div>
+            </div>
+            <i className = "trash icon"></i>
+            </div>
+        );
+    });
+
+    return <div className = "celled list">
+        {renderGroupList}
+    </div>
+};
+
+
+export default GroupList;
